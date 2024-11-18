d3be86fd4a2055f5259ae9f96ffdcded4fab7383
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-11-18 22:46:22 +0100

    Require api key for requests

--- main.js
+++ main.js
@@ -2,17 +2,26 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+    const apiKey = req.query.api_key;
+    if (!apiKey || apiKey !== process.env.API_KEY) {
+        res.status(401).json({ message: 'Unauthorized: Invalid or missing API key' });
+    }
+    next();
+});
 
 mongoose.connect(process.env.MONGO_URI, {});
 const db = mongoose.connection;
 
 const wordSchema = new mongoose.Schema({
-    _id: String,
+    _id: Number,
     word: String,
-});
+}, { collection: 'Words' });
 
 const Word = mongoose.model('Word', wordSchema);
 
@@ -27,7 +36,7 @@ app.get('/Words', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('API up and running!');
+    res.send('API up and running...');
 });
 
 db.once('open', () => {
